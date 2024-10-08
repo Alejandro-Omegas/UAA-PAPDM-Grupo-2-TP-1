03ac70a46c36e79c2735e54ea608ea55ee5ac374
Alejandro Arriola <arbaad@protonmail.com>
2024-10-07 22:01:02 -0300

    agregado data class auto

---
+++ app/src/main/java/com/alejandroarriola/tpgrupo2/Auto.kt
@@ -0,0 +1,10 @@
+package com.alejandroarriola.tpgrupo2
+
+data class Auto(
+    val marca: String,
+    val modelo: String,
+    val anio: Int,
+    val precio: Double,
+    val kilometraje: Int,
+    val imagenUrl: String
+)
