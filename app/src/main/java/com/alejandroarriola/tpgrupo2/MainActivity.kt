f7100f7c57e2d3058a475d5420ef8994b9b6906f
Alejandro Arriola <arbaad@protonmail.com>
2024-10-14 19:32:15 -0300

    added light and dark theme

--- app/src/main/java/com/alejandroarriola/tpgrupo2/MainActivity.kt
+++ app/src/main/java/com/alejandroarriola/tpgrupo2/MainActivity.kt
@@ -4,28 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.alejandroarriola.tpgrupo2.ui.theme.Tpgrupo2Theme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +17,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Tpgrupo2Theme {
-                PantallaRegistro()
+                //el surface contiene el color de fondo, por lo que es necesario para apreciar el dark mode en su totalidad
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    PantallaRegistro()
+                }
             }
         }
     }
@@ -44,6 +32,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     Tpgrupo2Theme {
+        PantallaRegistro()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingDarkPreview() {
+    Tpgrupo2Theme(darkTheme = true) {
         PantallaRegistro()
     }
 }
